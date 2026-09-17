0187ff81941b0eb4a1b040a2b47e18fe2f2c21bd
karrol-ke <326079230+karrol-ke@users.noreply.github.com>
2026-09-17 22:54:18 +0530

    encrypt and decrypt added in Cryptographic.java

--- app/src/main/java/com/white_vault/app/Cryptographic.java
+++ app/src/main/java/com/white_vault/app/Cryptographic.java
@@ -1,8 +1,15 @@
 package com.white_vault.app;
 
+import android.util.Base64;
+
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 
 public class Cryptographic {
@@ -12,6 +19,7 @@ public class Cryptographic {
      * @param password The string to hash
      * @return The 64-character hexadecimal SHA-256 hash
      */
+
     public String hashPassword(String password) {
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
@@ -28,6 +36,45 @@ public class Cryptographic {
             return hexString.toString();
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException("SHA-256 algorithm not found", e);
+        }
+    }
+
+    public static String encrypt(String text, String key) {
+
+        try {
+            byte[] keyBytes = key.getBytes(StandardCharsets.UTF_8);
+
+            SecretKeySpec secretKey = new SecretKeySpec(keyBytes, "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            byte[] encrypted = cipher.doFinal(text.getBytes(StandardCharsets.UTF_8));
+
+            return Base64.encodeToString(encrypted, Base64.NO_WRAP);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public static String decrypt(String encryptedText, String key) {
+
+        try {
+            byte[] keyBytes = key.getBytes(StandardCharsets.UTF_8);
+
+            SecretKeySpec secretKey = new SecretKeySpec(keyBytes, "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+
+            byte[] encrypted = Base64.decode(encryptedText, Base64.NO_WRAP);
+            byte[] decrypted = cipher.doFinal(encrypted);
+
+            return new String(decrypted, StandardCharsets.UTF_8);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 
