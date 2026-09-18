3c026f622a70ad2148c4e010b6a7923d40b84ba1
karrol-ke <326079230+karrol-ke@users.noreply.github.com>
2026-09-18 23:02:44 +0530

    Main activity updated

--- app/src/main/java/com/white_vault/app/MainActivity.java
+++ app/src/main/java/com/white_vault/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.white_vault.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +23,7 @@ import org.jetbrains.annotations.NotNull;
 public class MainActivity extends AppCompatActivity implements DefaultLifecycleObserver {
     DataBaseManager dataBaseManager = new DataBaseManager();
     MaterialCardView card_password, card_document;
+    TextView text_user_name;
     Intent intent;
     String user_name;
 
@@ -36,10 +38,13 @@ public class MainActivity extends AppCompatActivity implements DefaultLifecycleO
             return insets;
         });
 
-        user_name =
+        user_name = JsonHandler.getValue(this, "app_data.json", "user_info", "user_name");
 
+        text_user_name = findViewById(R.id.text_user_name);
         card_password = findViewById(R.id.card_password);
         card_document = findViewById(R.id.card_document);
+
+        text_user_name.setText(user_name);
 
         card_password.setOnClickListener(new View.OnClickListener() {
             @Override
