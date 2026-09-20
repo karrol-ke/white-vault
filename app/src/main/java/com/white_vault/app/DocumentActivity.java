9d18aff50a0fa7dab5830d15cda2b0670cc7371a
karrol-ke <326079230+karrol-ke@users.noreply.github.com>
2026-09-20 20:55:40 +0530

    DocumentActivity.java updated

--- app/src/main/java/com/white_vault/app/DocumentActivity.java
+++ app/src/main/java/com/white_vault/app/DocumentActivity.java
@@ -274,7 +274,7 @@ public class DocumentActivity extends AppCompatActivity {
         descriptionView.setTextSize(16);
 
         TextView dataView = new TextView(this);
-        dataView.setText("Decode size: " + document.data.length() + " characters");
+        dataView.setText("Encode size: " + document.data.length() + " characters");
         dataView.setTextColor(Color.parseColor("#A1A1AA"));
         dataView.setTextSize(13);
 
